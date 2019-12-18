9626aa64b4eafc225c8b74f36d226c148c881a01
Swarupa Popuri <swarupakarnam@yahoo.com>
2019-12-18 07:17:52 -0500

    Partially completed employer dashboard

--- assets/javaScript/logic.js
+++ assets/javaScript/logic.js
@@ -1,3 +1,4 @@
+ // $(document).ready(function () {
 var firebaseConfig = {
     apiKey: "AIzaSyA7l_OBpsdhvzfh4IyVSzBwMNnOc8gmJ2g",
     authDomain: "parttimegigs-10b82.firebaseapp.com",
@@ -5,7 +6,7 @@ var firebaseConfig = {
     projectId: "parttimegigs-10b82",
     storageBucket: "parttimegigs-10b82.appspot.com",
     messagingSenderId: "611238406430",
-    appId: "1:611238406430:web:f7cfd459d83705b67d810f"
+    appId: "1:611238406430:web:f7cfd459d83705b67d810f",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -18,18 +19,18 @@ var database = firebase.database();
 // Click Button changes what is stored in firebase
 //guy's code
 
-
-// mapboxgl.accessToken = 'pk.eyJ1IjoiZ3V5eWFmZmVhciIsImEiOiJjazQ2NDZucnUwZ2F6M2VuNjI3cDliZXl6In0.plk0zq29BJttq6ylX-85bA';
-// var map = new mapboxgl.Map({
-// container: 'map',
-// style: 'mapbox://styles/mapbox/streets-v11'
-// }); 
-
+if($("#map").length !== 0){
+  mapboxgl.accessToken ='pk.eyJ1IjoiZ3V5eWFmZmVhciIsImEiOiJjazQ2NDZucnUwZ2F6M2VuNjI3cDliZXl6In0.plk0zq29BJttq6ylX-85bA';
+  var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+  }); 
+};
 
 
 $("#submit-employersForm").on("click", function (event) {
     // Prevent the page from refreshing
-    event.preventDefault();
+     event.preventDefault();//<-hold information
 
     // Get inputs
     name = $("#name-input ").val().trim();
@@ -52,10 +53,23 @@ $("#submit-employersForm").on("click", function (event) {
         suggestedPrice: suggestedPrice,
         hourDaily: hourDaily,
         contact: contact,
-        description: description
+        description: description,
+        status: "active"
     }
-    database.ref("/jobDetails").push(newJob);
     console.log(newJob);
+    console.log("line works!")
+
+    var newJobKey = database.ref("/jobDetails").push(newJob).key;
+    
+    setTimeout(function(){ 
+      var updates = {};   
+      updates["/" + newJobKey + "/status"] = "expired";
+      database.ref("/jobDetails").update(updates);
+      console.log("timeout");
+      // database.ref("/expired/"+newJobKey).set(newJob);
+      // database.ref("/jobDetails/"+newJobKey).remove();
+    },3*60*60*1000);
+
 });
 //Grace - When a user posts a new job, take snapshot of the new data added
 database.ref("/jobDetails").on("child_added", function (snapShot) {
@@ -67,252 +81,184 @@ database.ref("/jobDetails").on("child_added", function (snapShot) {
     var buttonBid = snapShot.val().buttonBid;
 
     console.log(snapShot.val())
+    var button = $("<button type='button' id='bidButton' class='btn btn-outline-primary'>Bid</button>").html(buttonBid);
     var newRow = $("<tr>").append(
         $("<td>").text(jobTitle),
         $("<td>").text(city),
         $("<td>").text(description),
         $("<td>").text(suggestedPrice),
-        $("<button type='button' id='bidButton' class='btn btn-outline-primary'>Bid</button>").html(buttonBid)
+        button
     );
+    newRow.attr('data-address',city);
+    // newRow.attr("data-fireBaseRef", snapshot.ref());
     $("#partTime-table > tbody").append(newRow);
-    
+});  
+
+// set the bounds of the map
+// var bounds = [[-73.988154, 40.719929],[-73.9893433, 40.7426854]];
+// map.setMaxBounds(bounds);
+
+// an arbitrary start will always be the same
+// only the end or destination will change
+// var start = [-122.662323, 45.523751];
+
+// create a function to make a directions request
+function getRoute(start, end) {
+  // make a directions request using cycling profile
+  // an arbitrary start will always be the same
+  // only the end or destination will change
+  // var start = [-122.662323, 45.523751];
+  var url = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+
+  // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+  var req = new XMLHttpRequest();
+  req.responseType = 'json';
+  req.open('GET', url, true);
+  req.onload = function() {
+    var data = req.response.routes[0];
+    var route = data.geometry.coordinates;
+    var geojson = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'LineString',
+        coordinates: route
+      }
+    };
+    // if the route already exists on the map, reset it using setData
+    if (map.getSource('route')) {
+      map.getSource('route').setData(geojson);
+    } else { // otherwise, make a new request
+      map.addLayer({
+        id: 'route',
+        type: 'line',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: geojson
+            }
+          }
+        },
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#3887be',
+          'line-width': 5,
+          'line-opacity': 0.75
+        }
+      });
+    }
+    // add turn instructions here at the end
+  };
+  req.send();
+}
+
+if($("#map").length !== 0){
+  map.on('load', function() {
+
+  var coord1= []
+  var coord2 = []
+
+  $("#GoogleMapButton").on("click", function () {
+    var address = "105 Rivington St,New York,NY 10002"; 
+    var address1 = encodeURI(address)
+    var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+address1+".json?bbox=-171.791110603,18.91619,-66.96466,71.3577635769&access_token=pk.eyJ1IjoiZ3V5eWFmZmVhciIsImEiOiJjazQ2NDZucnUwZ2F6M2VuNjI3cDliZXl6In0.plk0zq29BJttq6ylX-85bA";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var results = response.features[0].center[0];
+        var results2 = response.features[0].center[1];
+        coord1.push(results,results2);
+        console.log("i hate aPI ",coord1);
+    })
+    address2 = "16 Madison Square West,New York,NY 10010" ;
+    address3 = encodeURI(address2)
+    var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+address3+".json?bbox=-171.791110603,18.91619,-66.96466,71.3577635769&access_token=pk.eyJ1IjoiZ3V5eWFmZmVhciIsImEiOiJjazQ2NDZucnUwZ2F6M2VuNjI3cDliZXl6In0.plk0zq29BJttq6ylX-85bA";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var results = response.features[0].center[0];
+        var results2 = response.features[0].center[1];
+        coord2.push(results,results2);
+        console.log("i hate aPI ",coord2);
+    })
+
+    setTimeout(function() {
+      getCompleteRoute(coord1,coord2)
+    }, 3000);
+  });
+
+
+function getCompleteRoute(start,end) {
+  console.log('start',start, 'end',end)
+  
+  // add start point
+  map.addLayer({
+    id: 'point',
+    type: 'circle',
+    source: {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [{
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'Point',
+            coordinates: start
+          }
+        }
+        ]
+      }
+    },
+    paint: {
+      'circle-radius': 10,
+      'circle-color': '#3887be'
+    }
+  });
+
+  // add end point
+  map.addLayer({
+    id: 'end',
+    type: 'circle',
+    source: {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [{
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'Point',
+            coordinates: end
+          }
+        }]
+      }
+    },
+    paint: {
+      'circle-radius': 10,
+      'circle-color': '#f30'
+    }
+  });
+
+
+  getRoute(start, end);
+  }
 });
+}
 
-
-
-// var coord1= []
-// var coord2 = []
-// $("#GoogleMapButton").on("click", function () {
-//     var address = "105 Rivington St,New York,NY 10002"; 
-   
-//     var zipcode2 = 11019;
-//     var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+address+".json?bbox=-171.791110603,18.91619,-66.96466,71.3577635769&access_token=pk.eyJ1IjoiZ3V5eWFmZmVhciIsImEiOiJjazQ2NDZucnUwZ2F6M2VuNjI3cDliZXl6In0.plk0zq29BJttq6ylX-85bA";
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response) {
-//         console.log(response);
-//         var results = response.features[0].center[0];
-//         var results2 = response.features[0].center[1];
-//         coord1.push(results,results2);
-//         console.log("i hate aPI ",coord1);
-//     })
-//     address2 = "16 Madison Square West,New York,NY 10010" 
-//     var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+address2+".json?bbox=-171.791110603,18.91619,-66.96466,71.3577635769&access_token=pk.eyJ1IjoiZ3V5eWFmZmVhciIsImEiOiJjazQ2NDZucnUwZ2F6M2VuNjI3cDliZXl6In0.plk0zq29BJttq6ylX-85bA";
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response) {
-//         console.log(response);
-//         var results = response.features[0].center[0];
-//         var results2 = response.features[0].center[1];
-//         coord2.push(results,results2);
-//         console.log("i hate aPI ",coord2);
-//     })
-//     console.log(coord1,coord2);
-// });
-
-// // getRoute(coord1,coord2);
-// function getRoute(start,end) {
-//     // make a directions request using cycling profile
-//     // an arbitrary start will always be the same
-//     // only the end or destination will change
-//     // var start = [-122.662323, 45.523751];
-//     var url = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=pk.eyJ1IjoiZ3V5eWFmZmVhciIsImEiOiJjazQ2NDZucnUwZ2F6M2VuNjI3cDliZXl6In0.plk0zq29BJttq6ylX-85bA';
-  
-//     // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-//     var req = new XMLHttpRequest();
-//     req.responseType = 'json';
-//     req.open('GET', url, true);
-//     req.onload = function() {
-//       var data = req.response.routes[0];
-//       var route = data.geometry.coordinates;
-//       var geojson = {
-//         type: 'Feature',
-//         properties: {},
-//         geometry: {
-//           type: 'LineString',
-//           coordinates: route
-//         }
-//       };
-//       // if the route already exists on the map, reset it using setData
-//       if (map.getSource('route')) {
-//         map.getSource('route').setData(geojson);
-//       } else { // otherwise, make a new request
-//         map.addLayer({
-//           id: 'route',
-//           type: 'line',
-//           source: {
-//             type: 'geojson',
-//             data: {
-//               type: 'Feature',
-//               properties: {},
-//               geometry: {
-//                 type: 'LineString',
-//                 coordinates: geojson
-//               }
-//             }
-//           },
-//           layout: {
-//             'line-join': 'round',
-//             'line-cap': 'round'
-//           },
-//           paint: {
-//             'line-color': '#3887be',
-//             'line-width': 5,
-//             'line-opacity': 0.75
-//           }
-//         });
-//       }
-//       // add turn instructions here at the end
-//     };
-//     req.send();
-//   }
-  
-//   map.on('load', function() {
-//     // make an initial directions request that
-//     // starts and ends at the same location
-//     getRoute(coord1,coord2);
-  
-//     // Add starting point to the map
-//     map.addLayer({
-//       id: 'point',
-//       type: 'circle',
-//       source: {
-//         type: 'geojson',
-//         data: {
-//           type: 'FeatureCollection',
-//           features: [{
-//             type: 'Feature',
-//             properties: {},
-//             geometry: {
-//               type: 'Point',
-//               coordinates: start
-//             }
-//           }
-//           ]
-//         }
-//       },
-//       paint: {
-//         'circle-radius': 10,
-//         'circle-color': '#3887be'
-//       }
-//     });
-//     // this is where the code from the next step will go
-//   });
-
-
-// $("#submit-checkEmployerGigs").on("click", function (event) {
-//   event.preventDefault();
-//   const databaseRef = firebase.database().ref("jobDetails");
-//   var empName = $("#employerEmail").val().trim();
-  
-//   databaseRef.orderByChild("name").equalTo(empName).on("child_added", function (snapshot) {
-//     var gigsArray = [];
-//     gigsArray.push(snapshot.val());
-//     console.log(gigsArray);
-//     $("#myGigs").prepend(welcomeUser);
-//     for (var i = 0; i < gigsArray.length; i++) {
-
-//               console.log("User name is "+gigsArray[i].name);
-
-//       //Clicking on a view bids button opens all the bids associated with your gig. 
-//       $("#viewBidsBtn").on("click", function () {
-//         document.location.href = "gigBids.html";
-//       });
-//       //Clicking on edit my Gig button will gives the user options to edit their gig.
-//       $("#editGigBtn").on("click", function () {
-//         console.log("User name is "+gigsArray[i].name);
-//         $("#user-name").val(gigsArray[i].name);
-//         $("#job-title").html(gigsArray[i].jobTitle);
-//         $("#job-on-date").html(gigsArray[i].datepicker);
-//         $("#job-at-time").html(gigsArray[i].timePicker)
-//         $("#user-email").html(gigsArray[i].contact);
-//       });
-//     }
-//   });
-// })
-
-// var databaseRef = firebase.database().ref("jobDetails").orderByKey();
-
-// $("#submit-checkEmployerGigs").on("click", function (event) {
-//     event.preventDefault();
-//     var empName = $("#employerEmail").val().trim();
-//     document.location.href = "myGigs.html?user=" + empName;
-// });
-
-
-// $(".editGigBtn").click(function(){
-//   console.log('TEST TEST');
-// });
-
-// setInterval(() => {
-//   if (!$('#editGigModal').is(':visible')) return;
-
-//   console.log('sss')
-// })
-
-// $(document).on("show.bs.modal", "#editGigModal", function () { 
-//   console.log('Hi');
-// });
-
-// $(document).on('show.bs.modal','#editGigModal', function () {
-//   alert('hi');
-// })
-
-// $("#editGigModal").on('show', function () {
-//   console.log('edit modal loaded');
-//   var dataRefId = $(this).attr("data-fbref");
-//   console.log("Hi i am Active")
-//   // // $("#editGigModal").modal().on('show', function(){
-//   // //   databaseRef.child(buttonRef).on("child_added", function (snapshot1) {
-//   // //     console.log(snapshot1.val());
-//   // });
-// // });
-  
-// });
-
-// $(".editGigBtn").on("click", function () {
-//   console.log("Test");
-//   var dataRefId = $(this).attr("data-fbref");
-//   $.ajax({
-//     url: 'dataRetriever.js',
-//     type: 'post',
-//     data: {dataRefId: dataRefId},
-//     success: function(response){ 
-//       // Add response in Modal body
-//       $('.modal-body').html(response);
-
-//       // Display Modal
-//       $('#empModal').modal('show'); 
-//     }
-//     });
-//  });
-
-// $("button[data-fbref]").on("click", function () {
-//   console.log("Test");
-//   var buttonRef = $(this).attr("data-fbref");
-//   console.log(buttonRef);
-// });
-
-
-
-        //Clicking on edit my Gig button will gives the user options to edit their gig.
-
-  //   var welcomeUser = $("<h3>").html("Welcome back " + empName);
-  //   databaseRef.orderByChild("name").equalTo(empName).on("child_added", function (snapshot) {
-
-    // var query = 
-    // query.once("value")
-    //   .then(function(snapshot) {
-    //     snapshot.forEach(function(childSnapshot) {
-    //       // key will be "ada" the first time and "alan" the second time
-    //       var key = childSnapshot.key;
-    //       // childData will be the actual contents of the child
-    //       var childData = childSnapshot.val();
-    //   });
-    // });
-    $(document).on('click', ".editGigBtn",function(){
+// Swarupa Popuri - Employer Dashboard code starts here. 
+$(document).on('click', ".editGigBtn",function(){
         $("#formDiv").hide();
         $("#myGigs").hide();
         $("#editingGig").show();
@@ -375,6 +321,8 @@ database.ref("/jobDetails").on("child_added", function (snapShot) {
          });
         
     });
+
+    
 
 
     $("#submit-checkEmployerGigs").on("click", function(event){
